c4fb8d70ee3ec6871ed5606152c4d5a4d1d4b3d4
Majed Alrogi <majed.alrogi@kaust.edu.sa>
2024-10-08 15:20:28 +0000

    added a util function that fills out a TaskInfo struct given a ncclInfo struct

---
+++ src/emulator/helper.h
@@ -0,0 +1,7 @@
+#include "emulator.h"
+
+/**
+ * This is a simple methood that takes a filled out ncclInfo struct and fills  out
+ * an empty TaskInfo struct
+*/
+void Info2Task(ncclInfo *info, TaskInfo *task);
