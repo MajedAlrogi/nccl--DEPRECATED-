41bcc07fc0031a5c63a71c846765dd68a4691d9b
Majed Alrogi <majed.alrogi@kaust.edu.sa>
2024-10-08 16:00:37 +0000

    implemented genAllEnvVars. Farhan, revisit once logging is implemented

---
+++ src/emulator/global.cc
@@ -0,0 +1,55 @@
+#include "align.h"
+#include "comm.h"
+#include "emulator.h"
+#include <assert.h>
+#include <cinttypes>
+#include <cstdio>
+#include <map>
+#include <math.h>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int KERNEL_BYPASS = -1;
+int NUM_NODES = -1;
+int MY_NODE = -1;
+Coordinator global_coordinator;
+EmuTopology global_topology;
+mutex emulator_lock;
+
+ncclResult_t getAllEnvVars() {
+  setbuf(stdout, NULL);
+  setbuf(stderr, NULL);
+  LOG_MOD(NCCL_MOD, "modGetAllEnvVars");
+  char *env = getenv("OMPI_COMM_WORLD_SIZE");
+  if (env == NULL) {
+    env = getenv("MOD_N_MPI_RANKS");
+  }
+  if (env == NULL) {
+    LOG_MOD(NCCL_LOG_ABORT, "Error: N_MPI_RANKS not set");
+    return ncclModError;
+  } else {
+    NUM_NODES = atoi(env);
+    LOG_MOD(NCCL_MOD, "MOD_N_MPI_RANKS=%d", NUM_NODES);
+  }
+  env = getenv("OMPI_COMM_WORLD_RANK");
+  if (env == NULL) {
+    env = getenv("MOD_MY_MPI_RANK");
+  }
+  if (env == NULL) {
+    LOG_MOD(NCCL_LOG_ABORT, "Error: MY_MPI_RANK not set");
+    return ncclModError; // TODO: ALI Alfarhan check the logging
+  } else {
+    MY_NODE = atoi(env);
+    LOG_MOD(NCCL_MOD, "MY_MPI_RANK=%d", MY_NODE);
+  }
+  env = getenv("MOD_KERNEL_BYPASS");
+  if (env == NULL) {
+    LOG_MOD(NCCL_MOD, "MOD_KERNEL_BYPASS not set, default to 0");
+    KERNEL_BYPASS = 0;
+  } else {
+    KERNEL_BYPASS = atoi(env);
+    LOG_MOD(NCCL_MOD, "MOD_KERNEL_BYPASS=%d", KERNEL_BYPASS);
+  }
+  return ncclSuccess;
+}
